b4779558d00e8ff14221169ab3320f14ae0aa3fb
Kasiru69 <saptaswadas69@gmail.com>
2025-02-13 10:46:58 +0530

    LeetCode 3066

---
+++ LeetCode_3066.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LeetCode_3066{
+    public int minOperations(int[] arr, int k) {
+        PriorityQueue<Long> pq=new PriorityQueue<>();
+        for(int it:arr) pq.add((long)it);
+        long ans=0;
+        while(pq.size()>0){
+            long it=pq.poll();
+            long a=it;
+            if(a>=k) break;
+            ans++;
+            if(pq.size()==0) break;
+            it=pq.poll();
+            long b=it;
+            if(b>=k) break;
+            long ok=Math.min(a,b)*2+Math.max(a,b);
+            pq.add(ok);
+        }
+        return (int)ans;
+    }
+    public static void main(String args[]){
+
+    }
+}
